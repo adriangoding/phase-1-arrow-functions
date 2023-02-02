fe957a427e7a67504ea93a20c97f54f07bcdba7b
Adrian Goding <godingadrian@gmail.com>
2023-02-02 12:22:26 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (parameter, parameter2) => parameter / parameter2;
+divide(2000,100);
+
+const square = (parameter) => parameter * parameter;
+square(5);
+
+const add = (a, b) => {
+    return a + b;
+  };
+  add(3, 4);
